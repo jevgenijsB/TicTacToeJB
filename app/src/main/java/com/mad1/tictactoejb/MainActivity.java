fc2ab964581c832e90d4ce892018ee7bfb5c79d5
Jevgenijs <K00232592@student.lit.ie>
2020-10-25 23:35:06 +0000

    Added menu bar

--- app/src/main/java/com/mad1/tictactoejb/MainActivity.java
+++ app/src/main/java/com/mad1/tictactoejb/MainActivity.java
@@ -1,14 +1,44 @@
 package com.mad1.tictactoejb;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private TextView text;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        text = (TextView) findViewById(R.id.textView);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.action_new_game:
+               text.setText("New Game");
+               break;
+
+            case R.id.action_settings:
+                text.setText("Settings");
+                break;
+
+            case R.id.action_exit:
+                text.setText("Exit");
+                break;
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
