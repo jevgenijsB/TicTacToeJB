a22180c1c521a8dbf0a6dfddd998cb6d44990dc0
Jevgenijs Bogdanovics <pivoljub78@mail.ru>
2020-10-28 20:36:07 +0000

    Added some code to main class

--- app/src/main/java/com/mad1/tictactoejb/MainActivity.java
+++ app/src/main/java/com/mad1/tictactoejb/MainActivity.java
@@ -6,15 +6,30 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    private TextView text;
+    private TextView text, row1Col1;
+    private TextView grid[][];
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         text = (TextView) findViewById(R.id.textView);
+        addListenerOnTextView();
+    }
+
+    public void addListenerOnTextView(){
+        row1Col1 = (TextView) findViewById(R.id.row1Col1);
+        row1Col1.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        row1Col1.setText("X");
+                    }
+                }
+        );
     }
 
     @Override
