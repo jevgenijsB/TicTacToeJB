37ae18a4084b2b5f4650467645d595c9815bfb52
Jevgenijs <K00232592@student.lit.ie>
2020-10-29 15:19:51 +0000

    Added some functions for display grid

--- app/src/main/java/com/mad1/tictactoejb/MainActivity.java
+++ app/src/main/java/com/mad1/tictactoejb/MainActivity.java
@@ -9,27 +9,77 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-    private TextView text, row1Col1;
-    private TextView grid[][];
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private TextView text;
+    private TextView[][] grid_TextView = new TextView[3][3];
+    private String[][] grid_String = new String[3][3];
+    private int i,a;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         text = (TextView) findViewById(R.id.textView);
-        addListenerOnTextView();
+        grid_TextView[0][0] = (TextView) findViewById(R.id.row1Col1);
+        grid_TextView[0][0].setOnClickListener(this);
+        grid_TextView[0][1] = (TextView) findViewById(R.id.row1Col2);
+        grid_TextView[0][1].setOnClickListener(this);
+        grid_TextView[0][2] = (TextView) findViewById(R.id.row1Col3);
+        grid_TextView[0][2].setOnClickListener(this);
+        grid_TextView[1][0] = (TextView) findViewById(R.id.row2Col1);
+        grid_TextView[1][0].setOnClickListener(this);
+        grid_TextView[1][1] = (TextView) findViewById(R.id.row2Col2);
+        grid_TextView[1][1].setOnClickListener(this);
+        grid_TextView[1][2] = (TextView) findViewById(R.id.row2Col3);
+        grid_TextView[1][2].setOnClickListener(this);
+        grid_TextView[2][0] = (TextView) findViewById(R.id.row3Col1);
+        grid_TextView[2][0].setOnClickListener(this);
+        grid_TextView[2][1] = (TextView) findViewById(R.id.row3Col2);
+        grid_TextView[2][1].setOnClickListener(this);
+        grid_TextView[2][2] = (TextView) findViewById(R.id.row3Col3);
+        grid_TextView[2][2].setOnClickListener(this);
+
     }
 
-    public void addListenerOnTextView(){
-        row1Col1 = (TextView) findViewById(R.id.row1Col1);
-        row1Col1.setOnClickListener(
-                new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        row1Col1.setText("X");
-                    }
-                }
-        );
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.row1Col1:
+                grid_String[0][0] = "X";
+                  display_Grid();
+                  break;
+            case R.id.row1Col2:
+                grid_String[0][1] = "X";
+                display_Grid();
+                break;
+            case R.id.row1Col3:
+                grid_String[0][2] = "X";
+                display_Grid();
+                break;
+            case R.id.row2Col1:
+                grid_String[1][0] = "X";
+                display_Grid();
+                break;
+            case R.id.row2Col2:
+                grid_String[1][1] = "X";
+                display_Grid();
+                break;
+            case R.id.row2Col3:
+                grid_String[1][2] = "X";
+                display_Grid();
+                break;
+            case R.id.row3Col1:
+                grid_String[2][0] = "X";
+                display_Grid();
+                break;
+            case R.id.row3Col2:
+                grid_String[2][1] = "X";
+                display_Grid();
+                break;
+            case R.id.row3Col3:
+                grid_String[2][2] = "X";
+                display_Grid();
+                break;
+        }
     }
 
     @Override
@@ -56,4 +106,15 @@ public class MainActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+    public void display_Grid(){
+        for (int i=0;i<3;i++){
+            for(int a=0;a<3;a++){
+                 grid_TextView[i][a].setText(grid_String[i][a]);
+            }
+        }
+    }
+
+
+
 }
